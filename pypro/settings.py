801d704da93b94ee25bebac38e1e949dffbcd801
Henriquelima1984 <henriquecorreia100@gmail.com>
2022-02-03 22:30:26 -0300

    Collectfast ok
    close #34

--- pypro/settings.py
+++ pypro/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
 ]
 
@@ -128,6 +129,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # Configuração Amazon S3
@@ -139,6 +146,7 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
+    COLLECTFAST_ENABLED = True
     AWS_DEFAULT_ACL = None
 
     # Staticfiles Assets
