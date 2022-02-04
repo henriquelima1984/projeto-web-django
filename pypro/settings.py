340128f7fbc1fb260efb0ecfbd41b0c0d1eeb0e6
Henriquelima1984 <henriquecorreia100@gmail.com>
2022-02-03 22:11:47 -0300

    staticfiles and mediafiles ok
    close #32

--- pypro/settings.py
+++ pypro/settings.py
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # Configuração Amazon S3
-if AWS_ACCESS_KEY_ID:  # no pragma: cover
+if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
