570f54ad2be3649e137ed3628aeb567a3ecb3b0d
Henriquelima1984 <henriquecorreia100@gmail.com>
2022-02-10 23:37:02 -0300

    Subindo Landing Page para Web
    close #83

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render # noqa
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse # noqa
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<html><body>Hello Django</body></html>')
+    return render(request, 'base/home.html')
