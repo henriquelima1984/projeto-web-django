006d0744aa87d19c48f68f079d936909b3b968f4
Henriquelima1984 <henriquecorreia100@gmail.com>
2022-02-17 16:43:21 -0300

    Fluxo e templates otimizados.
    close #105

--- aperitivos/urls.py
+++ aperitivos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from aperitivos.views import videos
+from aperitivos.views import videos, indice
 
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', videos, name='video'),
+    path('', indice, name='indice'),
 ]
--- aperitivos/views.py
+++ aperitivos/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
+
+
+dict_videos = [
+    Video('motivacao', 'Vídeo Aperitivo: Motivação', 677826562),
+    Video('instalacao-windows', 'Instalação Windows', 678404725),
+]
+
+videos_dict = {v.slug: v for v in dict_videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'dict_videos': dict_videos})
 
 
 def videos(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = videos_dict[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
